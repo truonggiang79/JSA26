fbd814b59ce5f0535e3e22009af14cc86098cdcf
truonggiang79 <69026061+truonggiang79@users.noreply.github.com>
2022-08-27 22:04:19 +0700

    Bvn
    Bài 2

---
+++ lesson 5/bvn/main.js
@@ -0,0 +1,15 @@
+setTimeout(function(){
+    console.log("Binz nói với anh");
+}, 1000);
+setTimeout(function(){
+    console.log("Million dollar flow");
+}, 2000);
+setTimeout(function(){
+    console.log("Thành công đang tới nhanh");
+}, 3000);
+setTimeout(function(){
+    console.log("Million dollar flow");
+}, 4000);
+setTimeout(function(){
+    console.log("Touliver makin' beat");
+}, 5000);
